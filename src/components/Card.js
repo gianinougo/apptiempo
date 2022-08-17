31baa3ea6cb3bb819bb030834dab198fac9cb433
gianinougo <84639491+gianinougo@users.noreply.github.com>
2022-08-17 18:09:18 +0200

    Add files via upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -51,7 +51,6 @@ const Card = ({loadingData, showData, weather, forecast}) => {
                                     <p className="card-date">{date}</p>
                                     <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
                                     <p className="card-desc"><img src={url} alt={weather.weather[0].description} />{(weather.weather[0].description).toUpperCase()}</p>
-                                    
                                     <img src= "https://images.pexels.com/photos/5641953/pexels-photo-5641953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="img-fluid rounded-start imagen" alt="city"></img>
                                 </div>
                                 <div className="col-md-8">
--- src/components/Form.js
+++ src/components/Form.js
@@ -19,7 +19,7 @@ const Form = ({newLocation}) => {
         <div className="container">
             <form onSubmit = {onSubmit}>
                 <div className="input-group mb-3 mx-auto">
-                    <input type="text" className="form-control" placeholder="Ingrese una ciudad" value={city} onChange={(e) => setcity(e.target.value)} />
+                    <input type="text" className="form-control" placeholder="Ciudad" value={city} onChange={(e) => setcity(e.target.value)} />
                     <button className="btn btn-primary input-group-text" type='submit'>Buscar</button>
                 </div>
             </form>
